4a3f48c432061bb54d890e9ef41fb4ff9d27d747
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-01-21 21:53:32 +0900

    20230121
    프로그래머스 1문제

--- Programmers/LV1/기사단원의_무기.py
+++ Programmers/LV1/기사단원의_무기.py
@@ -1,17 +1,19 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/136798
+
 def solution(number, limit, power):
     answer = 0
 
     def div(num):
         cnt = 0
-        n_half = num**(1/2)
-        for k in range(1, int(n_half)+1):
+        n_half = num ** (1 / 2)
+        for k in range(1, int(n_half) + 1):
             if num % k == 0:
                 cnt += 2
         if n_half == int(n_half):
             cnt -= 1
         return cnt
 
-    for i in range(1, number+1):
+    for i in range(1, number + 1):
         div_n = div(i)
         print(div_n)
         if div_n > limit:
@@ -21,4 +23,6 @@ def solution(number, limit, power):
 
     return answer
 
+
+print(solution(5, 3, 2))
 print(solution(10, 3, 2))
