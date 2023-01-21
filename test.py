13dfd3f1a7b2d479683f55fdede26001938199d3
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-01-21 20:20:39 +0900

    폴더 정리

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print(3^3)
+
+N = int(input())
+for _ in range(N):
+    A = int(input())
+    cnt = 0
+    lst = map(int, input().split())
+    for i in lst:
+        if i % 2 == 1:
+            cnt += 1
+    print(cnt)
