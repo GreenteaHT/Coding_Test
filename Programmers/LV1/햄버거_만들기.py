469cd7fe9db6a3aa4f474196b3c2c0843908ef75
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-03-22 17:29:56 +0900

    Update 햄버거_만들기.py

--- Programmers/LV1/햄버거_만들기.py
+++ Programmers/LV1/햄버거_만들기.py
@@ -1,3 +1,14 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/133502
+
 def solution(ingredient):
+    ing_str = ''.join(map(str, ingredient))
     answer = 0
+
+    while "1231" in ing_str:
+        ing_str = ing_str.replace("1231", '', 1)
+        answer += 1
     return answer
+
+# 입출력 예시
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
+print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1, 1, 2, 3, 1]))
