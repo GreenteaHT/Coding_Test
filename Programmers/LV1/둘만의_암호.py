7e26a271bdb671dab66784100fd88bd9f034d0d4
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-03-22 16:59:07 +0900

    Update 둘만의_암호.py

--- Programmers/LV1/둘만의_암호.py
+++ Programmers/LV1/둘만의_암호.py
@@ -1,11 +1,16 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/155652
 
 def solution(s, skip, index):
-    for i in skip:
-
+    alp_lst = [chr(i) for i in range(97, 123)]
     answer = ''
 
+    for i in skip:
+        alp_lst.remove(i)
+
+    alp_len = len(alp_lst)
+    for j in s:
+        answer += alp_lst[(alp_lst.index(j) + index) % alp_len]
     return answer
 
+# 입출력 예시
 print(solution("aukks", "wbqd", 5))
-print(ord('a'))
