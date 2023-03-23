5b21d9f8d08f76ac3a481dbb10cd5e0f64428d5e
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-03-23 17:30:54 +0900

    당구 연습 풀이

--- Programmers/LV2/당구_연습.py
+++ Programmers/LV2/당구_연습.py
@@ -5,23 +5,16 @@ def solution(m, n, startX, startY, balls):
 
     for i in balls:
         if startX == i[0]:
-            answer.append(min(startX + i[0], (2 * m - startX - i[0])) ** 2 + (startY - i[1]) ** 2)
+            answer.append(min(min(startX + i[0], (2 * m - startX - i[0])) ** 2 + (startY - i[1]) ** 2,
+                          (2 * n - startY - i[1]) ** 2 if startY >= i[1] else (startY + i[1]) ** 2))
         elif startY == i[1]:
-            answer.append((startX - i[0]) ** 2 + min(startY + i[1], (2 * n - startY - i[1])) ** 2)
+            answer.append(min((startX - i[0]) ** 2 + min(startY + i[1], (2 * n - startY - i[1])) ** 2,
+                              (2 * m - startX - i[0]) ** 2 if startX >= i[0] else (startX + i[0]) ** 2))
         else:
             answer.append(min((startX - i[0]) ** 2 + min(startY + i[1], (2 * n - startY - i[1])) ** 2,
                               min(startX + i[0], (2 * m - startX - i[0])) ** 2 + (startY - i[1]) ** 2))
-        # if startX + i[0] >= m:
-        #     if startY + i[1] >= n:
-        #         answer.append((2*m - startX - i[0])**2 + (2 * n - startY - i[1])**2)
-        #     else:
-        #         answer.append((2*m - startX - i[0])**2 + (startY + i[1])**2)
-        # else:
-        #     if startY + i[1] >= n:
-        #         answer.append((startX + i[0]) ** 2 + (2 * n - startY - i[1]) ** 2)
-        #     else:
-        #         answer.append((startX + i[0]) ** 2 + (startY + i[1]) ** 2)
     return answer
 
-
+# 입출력 예시
 print(solution(10, 10, 3, 7, [[7, 7], [2, 7], [7, 3]]))
+print(solution(10, 10, 5, 9, [[5, 8]]))
