349b4ff4d42901f808a226414a0a5185511c138c
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-03-27 21:49:35 +0900

    두문제 풀이
    공원 산책
    햄버거 만들기
    수정

--- Programmers/LV1/공원_산책.py
+++ Programmers/LV1/공원_산책.py
@@ -5,32 +5,32 @@ def solution(park, routes):
     W = len(park[0])
     dir_dic = {"E": [0, 1], "W": [0, -1], "N": [-1, 0], "S": [1, 0]}
 
-    yx = [0, 0]
+    cur_pos = None
     for i in range(W):
         for j in range(H):
             if park[j][i] == "S":
-                yx = [j, i]
+                cur_pos = [j, i]
                 break
+        if cur_pos is not None:
+            break
 
     for i in routes:
-        flg1 = False
         dir, dis = i.split(' ')
         dis = int(dis)
-        if 0 <= yx[1] + dir_dic[dir][1] * dis < W and 0 <= yx[0] + dir_dic[dir][0] * dis < H:
-            for x in range(yx[1]+1, yx[1] + dir_dic[dir][1] * dis + 1):
-                if not park[yx[0]][x] == "O":
-                    flg1 = True
-                    break
-            for y in range(yx[0]+1, yx[0] + dir_dic[dir][0] * dis + 1):
-                if not park[y][yx[1]] == "O":
+        new_pos = [cur_pos[0] + dir_dic[dir][0] * dis, cur_pos[1] + dir_dic[dir][1] * dis]
+
+        if 0 <= new_pos[1] < W and 0 <= new_pos[0] < H:
+            flg1 = False
+            for j in range(dis):
+                tmp_pos = [cur_pos[0] + dir_dic[dir][0] * (j+1), cur_pos[1] + dir_dic[dir][1] * (j+1)]
+                if park[tmp_pos[0]][tmp_pos[1]] == "X":
                     flg1 = True
                     break
             if not flg1:
-                yx[0] += dir_dic[dir][0] * dis
-                yx[1] += dir_dic[dir][1] * dis
-    return yx
+                cur_pos = new_pos
+    return cur_pos
 
 # 입출력 예시
-print(solution(["SOO","OOO","OOO"], ["E 2","S 2","W 1"]))
-print(solution(["SOO","OXX","OOO"], ["E 2","S 2","W 1"]	))
-print(solution(["OSO","OOO","OXO","OOO"], ["E 2","S 3","W 1"]))
+print(solution(["SOO", "OOO", "OOO"], ["E 2", "S 2", "W 1"]))
+print(solution(["SOO", "OXX", "OOO"], ["E 2", "S 2", "W 1"]))
+print(solution(["OSO", "OOO", "OXO", "OOO"], ["E 2", "S 3", "W 1"]))
--- Programmers/LV1/햄버거_만들기.py
+++ Programmers/LV1/햄버거_만들기.py
@@ -1,33 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/133502
 
-# def solution(ingredient):
-#     ing_str = ''.join(map(str, ingredient))
-#     answer = 0
-#
-#     while "1231" in ing_str:
-#         ing_str = ing_str.replace("1231", '', 1)
-#         answer += 1
-#     return answer
-
 def solution(ingredient):
-    answer = 0
-    for i in range(len(ingredient)):
-        flg1 = 0
-        n = 0
-        while n < len(ingredient):
-            if ingredient[n] == 1:
-                if ingredient[n + 1:n + 4] == [2, 3, 1]:
-                    answer += 1
-                    del ingredient[n: n + 4]
-                    n -= 1
-                    flg1 = 1
-                    break
-            n += 1
-        if not flg1:
-            break
-    return answer
+    ing_stk = []
+    n_hambuger = 0
+
+    for i in ingredient:
+        ing_stk.append(i)
+        if ing_stk[-4:] == [1, 2, 3, 1]:
+            n_hambuger += 1
+            del ing_stk[-4:]
+    return n_hambuger
 
 # 입출력 예시
 print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]))
-print(solution([2, 1, 1, 2, 3, 1, 2, 3, 1, 1, 2, 3, 1]))
+print(solution([1, 3, 2, 1, 2, 1, 3, 1, 2]))
 
