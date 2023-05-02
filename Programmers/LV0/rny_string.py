e3ec0099119ea297da5dbc53c38ac8570eb06f3f
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 22:29:15 +0900

    Create rny_string.py

---
+++ Programmers/LV0/rny_string.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181863
+
+def solution(rny_string):
+    return rny_string.replace('m', 'rn')
+  
+# 입출력 예시
+print(solution("masterpiece"))
+print(solution("programmers"))
+print(solution("jerry"))
+print(solution("burn"))
