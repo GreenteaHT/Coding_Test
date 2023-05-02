b2efd9be98e86db2971fabc6e537ee9e58f0920c
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 19:29:49 +0900

    Create 문자열_돌리기.py

---
+++ Programmers/LV0/문자열_돌리기.py
@@ -0,0 +1,13 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181945
+
+for i in input():
+    print(i)
+    
+# join 사용
+# print('\n'.join(input()))
+
+# replace 사용
+# print(input().replace('', '\n').lstrip('\n'))
+
+# 입출력 예시
+# abcde
