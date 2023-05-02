06e631a4ef0e265722328282b8716492bdb05027
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 19:46:32 +0900

    Update 문자열_돌리기.py

--- Programmers/LV0/문자열_돌리기.py
+++ Programmers/LV0/문자열_돌리기.py
@@ -8,6 +8,10 @@ for i in input():
 
 # replace 사용
 # print(input().replace('', '\n').lstrip('\n'))
+# print(input().replace('','\n')[1:])
+
+# list sep 사용
+# print(*[x for x in input()], sep='\n')
 
 # 입출력 예시
 # abcde
