c2f1daf0b560758abb83b00bd1ff27af6bb40f3c
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 17:10:47 +0900

    Update 기능개발.py

--- Programmers/LV2/기능개발.py
+++ Programmers/LV2/기능개발.py
@@ -5,3 +5,7 @@ from collections import deque
 def solution(progresses, speeds):
     answer = []
     return answer
+
+# 입출력 예시
+print(solution([93, 30, 55], [1, 30, 5]))
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1]))
