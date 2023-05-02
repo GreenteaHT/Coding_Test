e129cdc0c0a62b143b71de18ef0faed06bd1ebef
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 21:26:11 +0900

    Create 9로_나눈_나머지.py

---
+++ Programmers/LV0/9로_나눈_나머지.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181914
+
+def solution(number):
+    return int(number) % 9
+  
+# 입출력 예시
+print(solution("123"))
+print(solution("78720646226947352489"))
