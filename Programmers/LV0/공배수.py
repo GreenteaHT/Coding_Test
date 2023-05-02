d95049ff4a43597c9d164f6da6958c3d8e42d741
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 16:30:41 +0900

    Update 공배수.py

--- Programmers/LV0/공배수.py
+++ Programmers/LV0/공배수.py
@@ -1,6 +1,7 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181936
 
 def solution(number, n, m):
+    # 조건을 만족 하면 1을 반환, 만족 하지 못하면 0을 반환
     return int(number % n == 0 and number % m == 0)
 
 # 입출력 예시
