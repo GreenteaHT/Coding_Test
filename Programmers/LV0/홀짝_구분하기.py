d0c34a5286e48969e3668d0bc831084f5cd6c8fa
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2023-05-02 19:48:12 +0900

    Create 홀짝_구분하기.py

---
+++ Programmers/LV0/홀짝_구분하기.py
@@ -0,0 +1,12 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181944
+
+a = int(input())
+
+if a % 2 == 0:
+    print('%d is even' %a)
+else:
+    print('%d is odd' %a)
+    
+# 입출력 예시
+# 100
+# 1
