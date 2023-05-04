8fdff46011933509f50c4fd36275a929c10f7e9b
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-04 16:14:31 +0900

    Create 뒤에서_5등까지.py

---
+++ Programmers/LV0/뒤에서_5등까지.py
@@ -0,0 +1,7 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181853
+
+def solution(num_list):
+    return sorted(num_list)[:5]
+
+# 입출력 예시
+print(solution([12, 4, 15, 46, 38, 1, 14]))
