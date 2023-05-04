ca73f1dae5b8e24b4a0547d761d915e412f0e4ac
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-04 21:11:29 +0900

    Create n_번째_원소부터.py

---
+++ Programmers/LV0/n_번째_원소부터.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181892
+
+def solution(num_list, n):
+    return num_list[n-1:]
+
+# 입출력 예시
+print(solution([2, 1, 6], 3))
+print(solution([5, 2, 1, 7, 5], 2))
