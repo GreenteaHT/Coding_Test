3a5872eb3f5e80cf20ffaf35276997e6a39b6131
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-04 21:09:04 +0900

    Create 부분_문자열.py

---
+++ Programmers/LV0/부분_문자열.py
@@ -0,0 +1,8 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/181842
+
+def solution(str1, str2):
+    return int(str1 in str2)
+
+# 입출력 예시
+print(solution("abc", "aabcc"))
+print(solution("tbt", "tbbttb"	))
