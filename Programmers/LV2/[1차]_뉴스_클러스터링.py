de2891cbb43a742074a98a8abfd8f88a7b181935
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-04 23:04:40 +0900

    Create [1차]_뉴스_클러스터링.py

---
+++ Programmers/LV2/[1차]_뉴스_클러스터링.py
@@ -0,0 +1,16 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17677
+
+# 중복도 가능하게 수정 할 것
+
+def solution(str1, str2):
+    str1 = str1.upper()
+    str2 = str2.upper()
+    str1_set = set(str1[i]+str1[i+1] for i in range(len(str1) - 1) if
+                'A' <= str1[i] <= 'Z' and 'A' <= str1[i + 1] <= 'Z')
+    str2_set = set(str2[j] + str2[j + 1] for j in range(len(str2) - 1) if
+                'A' <= str2[j] <= 'Z' and 'A' <= str2[j + 1] <= 'Z')
+    print(str1_set, str2_set)
+    return int(len(str1_set & str2_set) / len(str1_set | str2_set) * 65536)
+
+# 입출력 예시
+print(solution('FRANCE', 'french'))
