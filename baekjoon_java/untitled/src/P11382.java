a7630305708618ae67777b342914ad745f535874
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-05 16:28:57 +0900

    Create P11382.java

---
+++ baekjoon_java/untitled/src/P11382.java
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/11382
+
+import java.util.Scanner;
+
+public class P11382 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        long A = sc.nextLong();
+        long B = sc.nextLong();
+        long C = sc.nextLong();
+        System.out.println(A + B + C);
+    }
+}
+
+// 입출력 예시
+// 77 77 7777
