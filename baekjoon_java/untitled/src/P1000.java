8b9d6c7847e4186cd8e6f39a67ab7db8e7367c5b
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-05 16:16:25 +0900

    create beakjoon_java dir

---
+++ baekjoon_java/untitled/src/P1000.java
@@ -0,0 +1,14 @@
+// https://www.acmicpc.net/problem/1000
+
+import java.util.*;
+public class P1000{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        System.out.println(A + B);
+    }
+}
+
+// 에제 입력
+// 1 2
