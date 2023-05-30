26a6ed087df3aadd8bbc006d3b84f67ec8165caa
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-05-30 09:04:25 +0900

    Programmers java 풀

---
+++ Programmers_java/src/Main.java
@@ -0,0 +1,9 @@
+import lv0.*;
+
+public class Main {
+    public static void main(String[] args)
+    {
+        P120802 a = new P120802();
+        System.out.println(a.solution(2, 3));
+    }
+}
---
+++ Programmers_java/src/lv0/P120802.java
@@ -0,0 +1,12 @@
+package lv0;
+
+public class P120802 {
+    public int solution(int num1, int num2) {
+        return num1 + num2;
+    }
+    public static void main(String[] args){
+        P120802 a = new P120802();
+        System.out.println(a.solution(2, 3));
+        System.out.println(a.solution(100, 2));
+    }
+}
