8a77cc1b331f1ced627834b121b50b080a16116c
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-08-28 23:28:44 +0900

    Update 기능개발.py

--- Programmers/LV2/기능개발.py
+++ Programmers/LV2/기능개발.py
@@ -1,11 +1,18 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/42586
 
-from collections import deque
+from math import ceil
 
 def solution(progresses, speeds):
-    answer = []
+    time_required = [ceil((100 - p) / s) for p, s in zip(progresses, speeds)]
+    tmp = time_required[0]
+    answer = [0]
+    for i in time_required:
+        if i > tmp:
+            tmp = i
+            answer.append(0)
+        answer[-1] += 1
     return answer
 
 # 입출력 예시
 print(solution([93, 30, 55], [1, 30, 5]))
-print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1]))
+print(solution([95, 90, 99, 99, 80, 99], [1, 1, 1, 1, 1, 1]))
