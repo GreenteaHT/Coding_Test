cf86ad65262c69398a94a0ef40ef49f2c25a28f0
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2023-08-30 15:32:20 +0900

    Update [1차]_뉴스_클러스터링.py
    문제 풀이 완료
    다른 풀이 참고해볼 것

--- Programmers/LV2/[1차]_뉴스_클러스터링.py
+++ Programmers/LV2/[1차]_뉴스_클러스터링.py
@@ -5,12 +5,16 @@
 def solution(str1, str2):
     str1 = str1.upper()
     str2 = str2.upper()
-    str1_set = set(str1[i]+str1[i+1] for i in range(len(str1) - 1) if
+    str1_set = set(str1[i] + str1[i + 1] for i in range(len(str1) - 1) if
                 'A' <= str1[i] <= 'Z' and 'A' <= str1[i + 1] <= 'Z')
     str2_set = set(str2[j] + str2[j + 1] for j in range(len(str2) - 1) if
                 'A' <= str2[j] <= 'Z' and 'A' <= str2[j + 1] <= 'Z')
     print(str1_set, str2_set)
-    return int(len(str1_set & str2_set) / len(str1_set | str2_set) * 65536)
+    return int(len(str1_set & str2_set) / 
+               len(str1_set | str2_set) * 65536) if str1_set | str2_set else 65536
 
 # 입출력 예시
 print(solution('FRANCE', 'french'))
+print(solution('handshake', 'shake hands'))
+print(solution('aa1+aa2', 'AAAA12'))
+print(solution('E=M*C^2', 'e=m*c^2'))
