cba0cb5a00269407e3925d88b4b2777de35002e6
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-03-05 23:34:55 +0900

    Create [PCCE_기출문제]_9번_이웃한_칸.py

---
+++ Programmers_Python/LV1/[PCCE_기출문제]_9번_이웃한_칸.py
@@ -0,0 +1,22 @@
+def solution(board, h, w):
+    # 1
+    n = len(board[0])
+    # 2
+    count = 0
+    # 3
+    dh = [0, 1, -1, 0]
+    dw = [1, 0, 0, -1]
+    # 4
+    for i in range(4):
+        # 4-1
+        h_check = h + dh[i]
+        w_check = w + dw[i]
+        # 4-2
+        if h_check >= 0 and h_check < n and w_check >= 0 and w_check < n:
+            # 4-2-a
+            if board[h][w] == board[h_check][w_check]:
+                count += 1
+    
+    return count
+
+print(solution([["blue", "red", "orange", "red"], ["red", "red", "blue", "orange"], ["blue", "orange", "red", "red"], ["orange", "orange", "red", "blue"]], 1, 1))
