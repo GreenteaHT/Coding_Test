33448c3d1b0d96b677d00579229c4e93370c5626
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-03-12 16:32:56 +0900

    Update ROOT_아이템_구하기.sql

---
+++ Programmers_SQL/LV2/ROOT_아이템_구하기.sql
@@ -0,0 +1,8 @@
+-- https://school.programmers.co.kr/learn/courses/30/lessons/273710
+
+SELECT A.ITEM_ID, A.ITEM_NAME
+FROM ITEM_INFO A
+    INNER JOIN ITEM_TREE B
+    ON A.ITEM_ID = B.ITEM_ID
+WHERE PARENT_ITEM_ID IS NULL
+ORDER BY ITEM_ID
