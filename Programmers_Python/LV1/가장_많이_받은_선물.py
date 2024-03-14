b06467edb955be8398e49fc814a3f3d1789b5112
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-03-14 17:21:24 +0900

    Create 가장_많이_받은_선물.py

---
+++ Programmers_Python/LV1/가장_많이_받은_선물.py
@@ -0,0 +1,10 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/258712
+
+def solution(friends, gifts):
+    answer = 0
+    return answer
+
+
+# 입출력 예시
+print(solution(["muzi", "ryan", "frodo", "neo"], ["muzi frodo", "muzi frodo", "ryan muzi", "ryan muzi", "ryan muzi", "frodo muzi", "frodo ryan", "neo muzi"]))
+print(solution(["joy", "brad", "alessandro", "conan", "david"],	["alessandro brad", "alessandro joy", "alessandro conan", "david alessandro", "alessandro david"]))
