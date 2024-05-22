24bfca5c46d532be7d12c1935d9ac353d6301961
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-05-22 16:32:50 +0900

    Update 배열_만들기_2.py

--- Programmers_Python/LV0/배열_만들기_2.py
+++ Programmers_Python/LV0/배열_만들기_2.py
@@ -1,26 +1,17 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/181921
 
 def solution(l, r):
-    r_lst = []
-    # r을 바이너리 값으로 변환 (5이상은 1, 5미만은 0)
-    str_r = str(r)
-    len_str_r = len(str_r)
-    r_binary = 2 ** len_str_r - 1
-    for n, i in enumerate(str_r):
-        if int(i) == 0:
-            r_binary -= 2 ** (len_str_r - n - 1)
-        elif int(i) < 5:
-            r_binary -= 2 ** (len_str_r - n - 1)
-            break
-        
-    for i in range(1, r_binary+1):
+    n_lst = []
+    n_binary = 2 ** len(str(r))
+    
+    # r을 바이너리 값으로 변환 (5 -> 1, 0 -> 0)
+    for i in range(1, n_binary):
         # 바이너리 값을 5로 이루어진 정수로 치환
-        case_5 = int(str(bin(i))[2:]) * 5
-        # l보다 크거나 같을 경우에만 추가
-        if case_5 >= l:
-            r_lst.append(case_5)
-    return r_lst or [-1]
+        case_5 = int(bin(i)[2:]) * 5
+        if case_5 >= l and case_5 <= r:
+            n_lst.append(case_5)
+    return n_lst or [-1]
 
-# 입출력 예시
-print(solution(5, 4500))
-print(solution(1, 100))
+# 테스트 예시
+print(solution(5, 555))  # [5, 50, 55, 500, 505, 550, 555]
+print(solution(10, 20))  # [-1]
