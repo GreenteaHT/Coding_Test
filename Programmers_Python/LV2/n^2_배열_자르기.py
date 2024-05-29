b340cb8c3eebce7a389bde995bf8895d6a52b558
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-05-29 23:44:25 +0900

    Update n^2_배열_자르기.py

--- Programmers_Python/LV2/n^2_배열_자르기.py
+++ Programmers_Python/LV2/n^2_배열_자르기.py
@@ -1,32 +1,11 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/87390
 
 def solution(n, left, right):
-    strat_row, start_point = divmod(left, n)
-    end_row, end_point = divmod(right, n)
-    height = end_row - strat_row + 1
-    length = right - left + 1
+    answer = [0] * (right - left + 1)
+    for i in range(left, right + 1):
+        answer[i - left] = max(i // n, i % n) + 1
+    return answer
 
-    n_lst = [0] * length
-
-    # 슬라이싱 하지 않는 방법으로 만들어야함
-    # 패턴이 있기에 max를 쓰지 않아도 될듯
-
-
-    # n_lst = [0] * (height * n)
-    # for i in range(height):
-    #     for j in range(n):
-    #         n_lst[n * i + j] = max(strat_row + i + 1, j + 1)
-
-    return n_lst[start_point:height*(n-1)+end_point+1]
-
-
-
-
-# 입출력 예시
+# 테스트
 print(solution(3, 2, 5))
 print(solution(4, 7, 14))
-
-
-123
-223
-333
