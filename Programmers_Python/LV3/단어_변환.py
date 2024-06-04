01a312913f02155212d48fde9a54c7fa78f52cb9
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-06-04 18:32:05 +0900

    Create 단어_변환.py

---
+++ Programmers_Python/LV3/단어_변환.py
@@ -0,0 +1,44 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/43163
+
+from collections import deque
+
+def solution(begin, target, words):
+    if target not in words:
+        return 0
+    
+    queue = deque([(begin, 0)])
+    visited = set(begin)
+    
+    while queue:
+        current_word, step = queue.popleft()
+        
+        if current_word == target:
+            return step
+        
+        for word in words:
+            if word not in visited and is_one_letter_diff(current_word, word):
+                visited.add(current_word)
+                queue.append((word, step + 1))
+
+    return 0
+
+
+def is_one_letter_diff(word1, word2):
+    cnt = 0
+    for spell1, spell2 in zip(word1, word2):
+        if spell1 != spell2:
+            cnt += 1
+        if cnt > 1:
+            return False
+    return cnt == 1
+
+# 테스트
+begin = "hit"
+target = "cog"
+words = ["hot", "dot", "dog", "lot", "log", "cog"]
+print(solution(begin, target, words))  # 출력: 4
+
+begin = "hit"
+target = "cog"
+words = ["hot", "dot", "dog", "lot", "log"]
+print(solution(begin, target, words))  # 출력: 0
