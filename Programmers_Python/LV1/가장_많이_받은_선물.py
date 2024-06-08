6b34b4b0030623644202b33a5a6e606d333f025d
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-06-08 18:12:28 +0900

    Update 가장_많이_받은_선물.py

--- Programmers_Python/LV1/가장_많이_받은_선물.py
+++ Programmers_Python/LV1/가장_많이_받은_선물.py
@@ -1,8 +1,27 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/258712
 
 def solution(friends, gifts):
-    answer = 0
-    return answer
+    n = len(friends)
+    dic = {key:i for i, key in enumerate(friends)}  # 각 친구들의 해시 값
+    exchange_table = [[0] * n for _ in range(n)]  # 주고받은 선물 표
+    present_indi = [0] * n  # 선물 지수 표
+    
+    for sending in gifts:
+        a, b = sending.split(' ')
+        exchange_table[dic[a]][dic[b]] += 1
+        present_indi[dic[a]] += 1
+        present_indi[dic[b]] -= 1
+
+    max_cnt = 0
+    for i in range(n):
+        cnt = 0
+        for j in range(n):
+            if exchange_table[i][j] - exchange_table[j][i] == 0 and present_indi[i] >  present_indi[j] or exchange_table[i][j] > exchange_table[j][i]:
+                cnt += 1
+        if cnt > max_cnt:
+            max_cnt = cnt
+
+    return max_cnt
 
 
 # 입출력 예시
