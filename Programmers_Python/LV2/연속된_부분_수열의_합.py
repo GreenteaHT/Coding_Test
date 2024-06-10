fb88c9dd58464374558c3cdf40af6c5aa485822d
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-06-10 15:25:00 +0900

    Update 연속된_부분_수열의_합.py

--- Programmers_Python/LV2/연속된_부분_수열의_합.py
+++ Programmers_Python/LV2/연속된_부분_수열의_합.py
@@ -1,20 +1,28 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/178870
 
 def solution(sequence, k):
-    seq_len = len(sequence)
-
-    for i in range(seq_len):
-        if sequence[seq_len - i] == k:
-            return [seq_len-i, seq_len-i]
-        elif sequence[seq_len - i] < k:
-            sequence = sequence[:seq_len-i]
-            seq_len = len(sequence)
-            break
-
-
-
-
+    length = len(sequence)
+    start, end = 0, 0  # 포인터 두개 사용
+    min_length = length + 1
+    alu = 0
     answer = []
+    
+    # k값 보다 작으면 수열의 뒷부분을 늘림
+    while end < length:
+        alu += sequence[end]
+        
+        # k값을 초과하면 수열의 앞부분을 줄임
+        while alu > k and start <= end:
+            alu -= sequence[start]
+            start += 1
+        
+        if alu == k:
+            if end - start + 1 < min_length:
+                min_length = end - start + 1
+                answer = [start, end]
+        
+        end += 1
+            
     return answer
 
 # 입출력 예시
