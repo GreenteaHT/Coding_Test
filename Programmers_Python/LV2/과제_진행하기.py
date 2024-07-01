dc229cf6506221508240dcc06be53ccf6f2225f5
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-07-01 17:55:02 +0900

    Update 과제_진행하기.py

--- Programmers_Python/LV2/과제_진행하기.py
+++ Programmers_Python/LV2/과제_진행하기.py
@@ -1,29 +1,44 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/176962
 
+from collections import deque
+
 def solution(plans):
-    scheduler = []
+    stack = deque([])
     answer = []
     plans = [[i[0], int(i[1][0:2]) * 60 + int(i[1][3:5]), int(i[2])] for i in plans]
     plans.sort(key=lambda x: x[1])
     print(plans)
+    
+    process = plans[0]
 
-    def chk_solution():
-        return 0
-
-    for i in range(len(plans)-1):
-        if plans[i+1][1] - plans[i][1] < plans[i][2]:
-            plans[i][2] -= plans[i + 1][1] - plans[i][1]
-            scheduler.append(plans[i])
-        else:
-            answer.append(plans[i][0])
-            if scheduler:
-                time = plans[i + 1][1] - plans[i][1] - plans[i][2]
-                for s in scheduler:
-                    if time > s[]
-
-
-
-    return plans
+    # 주어진 과제 진행
+    for i in range(1, len(plans)):
+        current_time = plans[i][1]
+        usable_time = current_time - process[1]
+        
+        if usable_time < process[2]:  # 현재 과제를 완료 하지 못할 때
+            stack.append([process[0], process[1], process[2] - usable_time])
+        else:  # 현재 과제를 완료 했을 떄
+            answer.append(process[0])
+            usable_time -= process[2]
+            # 남은 과제 풀기
+            while usable_time >= 0 and stack:
+                sub, start, time = stack.pop()
+                if usable_time < time: # 남은 과제도 풀지 못할 시
+                    stack.append([sub, start, time - usable_time])
+                else:  # 남은 과제를 풀었을 시
+                    answer.append(sub)
+                usable_time -= time
+        
+        process = plans[i]
+    
+    # 마지막 과제
+    answer.append(process[0])
+    # 남은 과제
+    while stack:
+        answer.append(stack.pop()[0])
+        
+    return answer
 
 
 # 입출력 예시
