8aa3c6405b3b397701c992274308d72ea4fd7a29
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-07-03 17:34:12 +0900

    Create [3차]_n진수_게임.py

---
+++ Programmers_Python/LV2/[3차]_n진수_게임.py
@@ -0,0 +1,32 @@
+# https://school.programmers.co.kr/learn/courses/30/lessons/17687
+
+def solution(n, t, m, p):
+    answer = ''
+    
+    
+    return answer
+
+# n진수, x번째의 숫자 찾기
+def find_n(n, x):
+    k = 1
+    a = n
+    x -= 1
+    while a <= x:
+        K += 1
+        x -= a
+        a = n**k
+    q, r = divmod(x, n)  # 몫과 나머지
+    # 더 작성
+    
+    return 0
+
+# 진수 변환
+def ten_to_n(n, q):
+    rev_base = ''
+    while n > 0:
+        n, mod = divmod(n, q)
+        rev_base += str(mod)
+        
+    return rev_base[::-1]
+        
+
