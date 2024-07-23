47aef432a0f8ecad8ef39980869d85c3405249ec
Greentea_latte <44830849+GreenteaHT@users.noreply.github.com>
2024-07-23 18:25:17 +0900

    Create 2703.js

---
+++ Leetcode_JS/2703.js
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/return-length-of-arguments-passed/
+
+var argumentsLength = function(...args) {
+    return args.length
+};
+
+// Test
+argumentsLength(1, 2, 3); // 3
