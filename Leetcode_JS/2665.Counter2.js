d0af2f87fae231a29b1813c47fea6d179c72cdcf
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2024-08-13 14:34:57 +0900

    Create 2665.Counter2.js

---
+++ Leetcode_JS/2665.Counter2.js
@@ -0,0 +1,43 @@
+// https://leetcode.com/problems/counter-ii/
+
+/**
+ * @param {integer} init
+ * @return { increment: Function, decrement: Function, reset: Function }
+ */
+var createCounter = function(init) {
+    const initValue = init;
+    var count = init;
+    
+    function increment() {
+      return ++count;
+    };
+
+    function reset() {
+        count = initValue;
+        return count;
+    };
+
+    function decrement() {
+        return --count;
+    };
+
+    return {
+        increment: increment,
+        reset:reset,
+        decrement: decrement
+    }
+};
+
+
+//Test
+const counter = createCounter(5)
+counter.increment(); // 6
+counter.reset(); // 5
+counter.decrement(); // 4
+
+
+// 
+/*
+function을 만들고 반환하는 대신에
+객체를 반환할 때 function을 넣어도됨ㅁ
+*/
