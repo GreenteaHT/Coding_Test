a73e21beca0325e63f70a2fe58b493318b4bfabb
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2025-04-12 15:17:26 +0900

    refactor(타겟_넘버): 변수명 정정 및 리스트 복사 방식 개선

--- Programmers_Python/LV2/타겟_넘버.py
+++ Programmers_Python/LV2/타겟_넘버.py
@@ -1,18 +1,16 @@
 # https://school.programmers.co.kr/learn/courses/30/lessons/43165
 
 def solution(numbers, target):
-    lst1 = [0]
-    lst2 = []
-    for i in numbers:
-        for j in lst1:
-            lst2.append(j + i)
-            lst2.append(j - i)
-        lst1 = lst2.copy()
-        lst2 = []
-        print(lst1)
-    print(lst1)
-    
-    return lst1.count(target)
+    current_sums = [0]
+    next_sums = []
+
+    for num in numbers:
+        for accumulated_sum in current_sums:
+            next_sums.append(accumulated_sum + num)
+            next_sums.append(accumulated_sum - num)
+        current_sums, next_sums = next_sums, []
+
+    return current_sums.count(target)
 
 # 입출력 예시
 print(solution([1, 1, 1, 1, 1], 3))
