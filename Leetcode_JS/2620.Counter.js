3a3279cc77b7849dd796e4bac03e2430b8ba65c3
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2025-04-28 00:25:01 +0900

    Update 2620.Counter.js

--- Leetcode_JS/2620.Counter.js
+++ Leetcode_JS/2620.Counter.js
@@ -4,14 +4,14 @@
  * @param {number} n
  * @return {Function} counter
  */
-var createCounter = function(n) {
-    return function() {
-        return n++;
-    };
+var createCounter = function (n) {
+  return function () {
+    return n++;
+  };
 };
 
 // 테스트
-const counter = createCounter(10)
-counter() // 10
-counter() // 11
-counter() // 12
+const counter = createCounter(10);
+counter(); // 10
+counter(); // 11
+counter(); // 12
