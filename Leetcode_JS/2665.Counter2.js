ec82bc7f40aec526307238326a645f711d362a42
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2025-04-28 15:57:58 +0900

    Update 2665.Counter2.js

--- Leetcode_JS/2665.Counter2.js
+++ Leetcode_JS/2665.Counter2.js
@@ -4,39 +4,37 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
-var createCounter = function(init) {
-    const initValue = init;
-    var count = init;
-    
-    function increment() {
-      return ++count;
-    };
+var createCounter = function (init) {
+  const initValue = init;
+  var count = init;
 
-    function reset() {
-        count = initValue;
-        return count;
-    };
+  function increment() {
+    return ++count;
+  }
 
-    function decrement() {
-        return --count;
-    };
+  function reset() {
+    count = initValue;
+    return count;
+  }
 
-    return {
-        increment: increment,
-        reset:reset,
-        decrement: decrement
-    }
+  function decrement() {
+    return --count;
+  }
+
+  return {
+    increment: increment,
+    reset: reset,
+    decrement: decrement,
+  };
 };
 
-
 //Test
-const counter = createCounter(5)
+const counter = createCounter(5);
 counter.increment(); // 6
 counter.reset(); // 5
 counter.decrement(); // 4
 
-
-// 
+//
 /*
 function을 만들고 반환하는 대신에
 객체를 반환할 때 function을 넣어도됨ㅁ
