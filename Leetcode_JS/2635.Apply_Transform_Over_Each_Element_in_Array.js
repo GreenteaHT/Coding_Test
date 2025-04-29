58b831796b27ae68e3e351d0bb1f1f86c40a990c
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2025-04-30 02:31:05 +0900

    Update 2635.Apply_Transform_Over_Each_Element_in_Array.js

--- Leetcode_JS/2635.Apply_Transform_Over_Each_Element_in_Array.js
+++ Leetcode_JS/2635.Apply_Transform_Over_Each_Element_in_Array.js
@@ -1,12 +1,10 @@
-// https://leetcode.com/problems/apply-transform-over-each-element-in-array/submissions/1379468701/?envType=study-plan-v2&envId=30-days-of-javascript
+// https://leetcode.com/problems/apply-transform-over-each-element-in-array/description
 
 /**
  * @param {number[]} arr
  * @param {Function} fn
  * @return {number[]}
  */
-var map = function(arr, fn) {
-    answer = []
-    arr.forEach((number, index) => answer.push(fn(number, index)))
-    return answer;
+var map = function (arr, fn) {
+  return arr.map((number, index) => fn(number, index));
 };
