ef8a25291ae96ae19f1241f252823ef631065e99
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2025-04-30 22:51:08 +0900

    Update 2634.Filter_Elements_from_Array.js

--- Leetcode_JS/2634.Filter_Elements_from_Array.js
+++ Leetcode_JS/2634.Filter_Elements_from_Array.js
@@ -5,14 +5,18 @@
  * @param {Function} fn
  * @return {number[]}
  */
-var filter = function(arr, fn) {
-    var arr2 = [];
-    // index도 사용하는 경우가 있기 때문에
-    // for문을 이용하여 하나씩 받아온 다음 조건문을 통해 arr2에 push를 진행
-    for (var i = 0; i < arr.length; i++) {
-        if (fn(arr[i], i)) {
-            arr2.push(arr[i]);
-        };
-    };
-    return arr2;
+var filter = function (arr, fn) {
+  return arr.filter((n, i) => fn(n, i));
 };
+
+// var filter = function(arr, fn) {
+//     var arr2 = [];
+//     // index도 사용하는 경우가 있기 때문에
+//     // for문을 이용하여 하나씩 받아온 다음 조건문을 통해 arr2에 push를 진행
+//     for (var i = 0; i < arr.length; i++) {
+//         if (fn(arr[i], i)) {
+//             arr2.push(arr[i]);
+//         };
+//     };
+//     return arr2;
+// };
