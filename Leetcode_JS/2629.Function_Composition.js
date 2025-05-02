0115b981d9adf36f507dd6fe9326801dcf46fe18
GreenteaHT <sh0946@gmail.com>
2025-05-03 00:08:39 +0900

    Update 2629.Function_Composition.js

--- Leetcode_JS/2629.Function_Composition.js
+++ Leetcode_JS/2629.Function_Composition.js
@@ -1,19 +1,15 @@
 // https://leetcode.com/problems/function-composition/description/
+
 /**
  * @param {Function[]} functions
  * @return {Function}
  */
-var compose = function(functions) {
-    
-    return function(x) {
-        for (let i = functions.length - 1; i >= 0; i--) {
-            x = functions[i](x);
-        }
-        return x;
-    }
+var compose = function (functions) {
+  return function (x) {
+    return functions.reduceRight((acc, fn) => fn(acc), x);
+  };
 };
 
 // 테스트
-const fn = compose([x => x + 1, x => 2 * x])
-fn(4) // 9
-
+const fn = compose([(x) => x + 1, (x) => 2 * x]);
+fn(4); // 9
