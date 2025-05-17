7cd5b6db72b52a727d4940575aafe6a51cef9afe
GreenteaHT <sh0946@gmail.com>
2025-05-17 23:58:05 +0900

    Update 2727.Is_Object_Empty.js

--- Leetcode_JS/2727.Is_Object_Empty.js
+++ Leetcode_JS/2727.Is_Object_Empty.js
@@ -1,11 +1,10 @@
+// https://leetcode.com/problems/is-object-empty
+
 /**
  * @param {Object|Array} obj
  * @return {boolean}
  */
 var isEmpty = function (obj) {
-  if (Array.isArray(obj)) {
-    return obj.length === 0;
-  } else {
-    return Object.keys(obj).length === 0;
-  }
+  for (const _ in obj) return false;
+  return true;
 };
