305d6a55947d0dbe51def1a570c11cbd87f8b269
GreenteaHT <44830849+GreenteaHT@users.noreply.github.com>
2025-05-20 14:46:29 +0900

    Update 2619.Array_Prototype_Last.js

--- Leetcode_JS/2619.Array_Prototype_Last.js
+++ Leetcode_JS/2619.Array_Prototype_Last.js
@@ -2,8 +2,8 @@
 /**
  * @return {null|boolean|number|string|Array|Object}
  */
-Array.prototype.last = function() {
-    return this.length ? this[this.length - 1] : -1;
+Array.prototype.last = function () {
+  return this.length ? this[this.length - 1] : -1;
 };
 
 // 테스트
